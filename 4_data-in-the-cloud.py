4632483992d35264404ada8acc8101d6de5d1024
Gonzalo Gamez <takosaga@gmail.com>
2025-07-16 12:01:52 +0300

    feat: finished learning with material

--- 4_data-in-the-cloud.py
+++ 4_data-in-the-cloud.py
@@ -66,5 +66,215 @@ def _(mo):
     return
 
 
+@app.cell
+def _(ambient_air_quality, con, mo):
+    _df = mo.sql(
+        f"""
+        describe sample_data.who.ambient_air_quality
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(ambient_air_quality, con, mo, sample_data):
+    _df = mo.sql(
+        f"""
+        SELECT 
+            year,
+            country_name,
+            AVG(pm10_concentration),
+            AVG(pm25_concentration)
+        FROM sample_data.who.ambient_air_quality
+        WHERE year >= (YEAR(current_date) - 10) and
+            country_name = 'Latvia'
+        GROUP BY year, country_name
+        ORDER BY year, country_name
+
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM read_parquet('hf://datasets/datonic/threatened_animal_species/data/threatened_animal_species.parquet');
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        USE my_db;
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    **Exercise 4.03**
+
+    Create a new table called `animals` in your MotherDuck database `md:my_db` based on the `datonic/threatened_animal_species` dataset.
+    """
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        CREATE TABLE IF NOT EXISTS animals AS (SELECT * FROM read_parquet('hf://datasets/datonic/threatened_animal_species/data/threatened_animal_species.parquet'))
+        """,
+        engine=con
+    )
+    return (animals,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    **Exercise 4.04**
+
+    DuckDB releases are each named after a duck! Let's load [this data](https://duckdb.org/data/duckdb-releases.csv) into a new table called `duckdb_ducks`. You can use `read_csv` to load the data directly from the HTTP URL: `https://duckdb.org/data/duckdb-releases.csv`.
+    """
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        CREATE TABLE IF NOT EXISTS duckdb_ducks AS (
+            SELECT *
+            FROM read_csv('https://duckdb.org/data/duckdb-releases.csv')
+        )
+        """,
+        engine=con
+    )
+    return (duckdb_ducks,)
+
+
+@app.cell
+def _(animals, con, mo):
+    _df = mo.sql(
+        f"""
+        DESCRIBE animals
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, duckdb_ducks, mo):
+    _df = mo.sql(
+        f"""
+        DESCRIBE duckdb_ducks;
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    **Exercise 4.05**
+
+    Create a new table called `duckdb_species` that joins the `duckdb_ducks` and `animals` tables on the scientific name.
+    """
+    )
+    return
+
+
+@app.cell
+def _(animals, con, duckdb_ducks, mo):
+    _df = mo.sql(
+        f"""
+        CREATE TABLE IF NOT EXISTS duckdb_species AS
+        (SELECT duck.*,
+            animals.category
+        FROM duckdb_ducks AS duck
+        INNER JOIN animals 
+        ON duck.duck_species_primary = animals.scientific_name)
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        ATTACH 'md:_share/mosaic_examples/b01cfda8-239e-4148-a228-054b94cdc3b4';
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        USE mosaic_examples;
+        SHOW TABLES;
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, mo, seattle_weather):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM seattle_weather
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        USE my_db;
+        DETACH mosaic_examples;
+        """,
+        engine=con
+    )
+    return
+
+
+@app.cell
+def _(con, mo):
+    _df = mo.sql(
+        f"""
+        DROP SHARE duck_share;
+        """,
+        engine=con
+    )
+    return
+
+
 if __name__ == "__main__":
     app.run()
